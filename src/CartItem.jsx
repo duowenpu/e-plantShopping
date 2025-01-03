2131d5ff642be20572a279be078f987cb684a5c3
puzeng@umich.edu <duowenpu@gmail.com>
2025-01-03 15:25:00 -0800

    deployment

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -10,7 +10,7 @@ const CartItem = ({ onContinueShopping }) => {
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
     let total = 0;
-    cart.forEach((item)=> total+=item.quantity)
+    cart.forEach((item)=> total+=item.quantity * parseFloat(item.cost.slice(1)));
     return total;
   };
 
